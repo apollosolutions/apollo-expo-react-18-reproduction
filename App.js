8e0b32a8d776b004cc749489c772df6b4b7033e9
Michael Edelman <me@apollographql.com>
2022-09-16 10:34:06 -0400

    minimal ac implementation

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import HomeScreen from "./src/HomeScreen";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text style={styles.title}>My Countries App</Text>
+        <HomeScreen styles={styles} />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderWidth: "5px",
+    display: "flex",
+    flex: "1",
+  },
+  item: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  modalView: {
+    width: "90%",
+    height: "90%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    paddingBottom: 10,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 5,
+    elevation: 2,
+    backgroundColor: "lightgrey",
   },
 });
---
+++ src/CountriesScreen.js
@@ -0,0 +1,53 @@
+import { Text, FlatList, Pressable, Modal, View } from "react-native";
+import { useQuery } from "@apollo/client";
+import { COUNTRY_QUERY } from "./gql/Query";
+
+const CountryItem = ({ country, styles }) => {
+  const { name, code } = country;
+  return (
+    <Pressable style={styles.item}>
+      <Text style={styles.header}>{name}</Text>
+    </Pressable>
+  );
+};
+
+export default function CountriesScreen({ styles, ...props }) {
+  const { data, loading } = useQuery(COUNTRY_QUERY, {
+    variables: {
+      code: props.code,
+    },
+  });
+
+  if (loading) {
+    return <Text>Fetching data...</Text>;
+  }
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={props.modalVisible}
+      onRequestClose={() => {
+        props.setModalVisible(!props.modalVisible);
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.modalView}>
+          <FlatList
+            data={data.continent.countries}
+            renderItem={({ item }) => (
+              <CountryItem country={item} styles={styles} />
+            )}
+            keyExtractor={(item, index) => index}
+          />
+          <Pressable
+            style={styles.button}
+            onPress={() => props.setModalVisible(!props.modalVisible)}
+          >
+            <Text style={styles.textStyle}>CLOSE</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
